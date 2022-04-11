4d985652be160c9e7b0d4a76730da5f599313656
Ashu-Tripathi <tripathiashu080@gmail.com>
2022-04-11 16:21:31 +0530

    res

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,4 @@
-const UserModel =require("../models/userModel")
-const aws = require("aws-sdk")
+const UserModel = require("../models/userModel")
 
 const isValid = function (value) {
   if (typeof (value) === 'undefined' || typeof (value) === 'null') {
@@ -14,44 +13,6 @@ const isValidPhoneNo=/^\+?([6-9]{1})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{5})$/
 const isValidPassword=/^[a-zA-Z0-9!@#$%^&*]{8,15}$/
 const isValidEmail=/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
 const isValidPinCode= /^[1-9]{1}[0-9]{2}\\s{0, 1}[0-9]{3}$/
-
-
-aws.config.update(
-    {
-        accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-        secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
-        region: "ap-south-1"
-    }
-)
-
-let uploadFile = async (file) => {
-    return new Promise( function(resolve, reject) {
-        //this function will upload file to aws and return the link
-        let s3 = new aws.S3({ apiVersion: "2006-03-01" }) 
-         await uploadFile(data[0])
-        var uploadParams = {
-            ACL: "public-read",
-            Bucket: "classroom-training-bucket", 
-            Key: "radhika/" + file.originalname, 
-            Body: file.buffer
-        }
-
-      s3.upload(uploadParams, function (err, data) {
-            if (err) { 
-                return reject({ "error": err }) 
-            }
-
-            console.log(data)
-            console.log(" file uploaded succesfully ")
-            return resolve(data.Location) // HERE
-          }
-        )
-
-    
-    }
-    )
-}
-
 
 
 const createUser = async (req, res) => {
@@ -71,15 +32,6 @@ const createUser = async (req, res) => {
             res.status(400).send({ status: false, message: 'please provide Email ID' })
             return
           }
-          if (!(isValidEmail.test(data.email))) {
-            res.status(400).send({ status: false, message: 'please provide valid Email ID' })
-            return
-          }
-          isEmailPresent = await UserModel.findOne({email:data.email})
-        if (isEmailPresent) {
-            res.status(400).send({ status: false, message: "This  is email already in use,please provide another email" })
-            return
-          }
         if (!isValid(data.profileImage)) {
             res.status(400).send({ status: false, message: 'please provide profile Image S3 Link' })
             return
@@ -88,21 +40,8 @@ const createUser = async (req, res) => {
             res.status(400).send({ status: false, message: 'please provide Mobile No.' })
             return
           }
-          if (!(isValidPhoneNo.test(data.phone))) {
-            res.status(400).send({ status: false, message: 'please provide valid Mobile no.' })
-            return
-          }
-          isPhonePresent = await UserModel.findOne({phone:data.phone})
-        if (isPhonePresent) {
-            res.status(400).send({ status: false, message: "This mobile is number already in use,please provide another mobile number" })
-            return
-          }
         if (!isValid(data.password)) {
             res.status(400).send({ status: false, message: 'please provide password' })
-            return
-          }
-          if (!(isValidPassword.test(data.password))) {
-            res.status(400).send({ status: false, message: 'please provide valid password(minLength=8 , maxLength=15)' })
             return
           }
         if (!isValid(data.address.shipping.street)) {
@@ -113,13 +52,8 @@ const createUser = async (req, res) => {
             res.status(400).send({ status: false, message: 'please provide shipping City name' })
             return
           }
-        if (!isValid(data.address.shipping.pincode)) {
+        if (isValid(data.address.shipping.pincode)) {
             res.status(400).send({ status: false, message: 'please provide shipping pincode' })
-            return
-          }
-          
-        if (!(isValidPinCode.test(data.address.shipping.pincode))) {
-            res.status(400).send({ status: false, message: 'please provide valid shipping Pincode.' })
             return
           }
         if (!isValid(data.address.billing.street)) {
@@ -130,16 +64,40 @@ const createUser = async (req, res) => {
             res.status(400).send({ status: false, message: 'please provide billing City name' })
             return
           }
-        if (!isValid(data.address.billing.bPincode)) {
+        if (isValid(data.address.billing.pincode)) {
             res.status(400).send({ status: false, message: 'please provide billing Pincode' })
+            return
+          }
+        if (!(isValidEmail.test(data.email))) {
+            res.status(400).send({ status: false, message: 'please provide valid Email ID' })
+            return
+          }
+        if (!(isValidPassword.test(data.password))) {
+            res.status(400).send({ status: false, message: 'please provide valid password(minLength=8 , maxLength=15)' })
+            return
+          }
+        if (!(isValidPhoneNo.test(data.phone))) {
+            res.status(400).send({ status: false, message: 'please provide valid Mobile no.' })
+            return
+          }
+        if (!(isValidPinCode.test(data.address.shipping.pincode))) {
+            res.status(400).send({ status: false, message: 'please provide valid shipping Pincode.' })
             return
           }
         if (!(isValidPinCode.test(data.address.billing.pincode))) {
             res.status(400).send({ status: false, message: 'please provide valid billing Pincode.' })
             return
           }
-        
-        
+        isPhonePresent = await UserModel.findOne({phone:data.phone})
+        if (isPhonePresent) {
+            res.status(400).send({ status: false, message: "This mobile is number already in use,please provide another mobile number" })
+            return
+          }
+        isEmailPresent = await UserModel.findOne({email:data.email})
+        if (isEmailPresent) {
+            res.status(400).send({ status: false, message: "This  is email already in use,please provide another email" })
+            return
+          }
 
         const createdUser = await UserModel.create(data)
         res.status(201).send({status:true, message:"User created successfully", data: createdUser })
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,55 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    fname: {
-        type: String,
-        required: true,
-        trim:true
-    },
-    lname: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true,
-        trim: true
-    },
-    profileImage: {
-        type:String, 
-        required:true
-    },
-    phone: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-        maxlength: 15
-    },
+const userSchema = new mongoose.Schema(
+  {
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    email: { type: String, required: true },
+    profileImage: { type: String, required: true }, // s3 link
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minLen: 8, maxLen: 15 }, // encrypted password
     address: {
-
-        shipping:{
-            street: {type:String, required:true},
-        city: {type:String, required:true},
-        pincode: {type:Number, required:true},
-        },
-
-        billing:{
-            street: {type:String, required:true},
-        city: {type:String, required:true},
-        pincode: {type:Number, required:true},
-        }
-        
+      shipping: {
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        pincode: { type: Number, required: true },
+      },
+      billing: {
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        pincode: { type: Number, required: true },
+      },
     },
-},
-    { timestamps: true });
-
-module.exports = mongoose.model('User', userSchema);
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("user", userSchema);
